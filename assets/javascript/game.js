6d5d1979296f8a30ff4b52162e20bdeb7cf6cbd3
Felipe Cantu <cantujr.felipe@gmail.com>
2020-02-23 12:15:14 -0600

    hopefully the last turn it
    no sure how to put this everything kinda of just lost it place from deleting repo so many times and I had to redo this homework so many times too alittle upset but hopefully this is finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,57 +2,65 @@
 window.onload = function() {
     // crystal images in array
 	var crystals = ['assets/images/amethystcrystal.png', 'assets/images/aquamarinecrystal.png', 'assets/images/citrinecrystal.png', 'assets/images/emeraldcrystal.png', 'assets/images/rubycrystal.png'];
-	// 
+	//sets wins and losses
 	var wins = 0;
-	var lose = 0;
+	var losses = 0;
 	var userscore = 0;
-	
-	var matchingscore = Math.floor((Math.random() * 100) + 20);
 
+	// random number generator for match the number
+	var matchingScore = Math.floor((Math.random() * 101) + 19);
+
+	// button for how to play
 	$('.gameinfo').hide();
 	$('#btn1').on('click', function(){
 		$('.gameinfo').toggle();		
 	});
 	
+	// text for match the number
+	$("#matchingscores").text(matchingScore);
 
-	$("#matchingscores").text(matchingscore);
-
+	// 
 	function restart() {
-		matchingscore = Math.floor((Math.random() * 100) + 20);
-		console.log(matchingscore);
-		$("#matchingscores").text(matchingscore);
-    // here you make a new colorcounts value but never put it anywhere
-		colorcounts = (Math.floor(Math.random() * 10) + 2);
+		 // Number should be between 19 - 120
+		matchingScore = Math.floor((Math.random() * 101) + 19);
+		console.log(matchingScore);
+		$("#matchingscores").text(matchingScore);
+		
+    	// Random number has to be between 1 - 12
+		crystalCount = (Math.floor(Math.random() * 11) + 1);
 		userscore = 0;
     $("#scores").text(userscore);
 	}
-
+	// random generator for crystals
     for (var i = 0; i < crystals.length; i++) {
-    	var colorcounts = Math.floor(Math.random() * 10) + 2;
-      	var imagecrystal = $("<img>");
-      	imagecrystal.attr("src", crystals[i]);
-      	imagecrystal.addClass("crystal-image");
-        // need to update this one each element
-      	imagecrystal.attr("data-crystalvalue", colorcounts);
-      	$("#colorcrystal").append(imagecrystal);
+    	var crystalCount = Math.floor(Math.random() * 11) + 1;
+      	var crystalImage = $("<img>");
+      	crystalImage.attr("src", crystals[i]);
+      	crystalImage.addClass("crystal-image");
+      	crystalImage.attr("data-crystalvalue", crystalCount);
+      	$("#crystals").append(crystalImage);
     }
 
+	 //  Decaring variables for score
     $(".crystal-image").on("click", function() {
 		var crystalValue = ($(this).attr("data-crystalvalue"));
       	crystalValue = parseInt(crystalValue);
-		    console.log(matchingscore);
+		    console.log(matchingScore);
       	userscore += crystalValue;
       	console.log(userscore);
-      	$("#scores").text(userscore);
-
-		if (userscore === matchingscore) {
+		  $("#scores").text(userscore);
+		  
+		// point tallies
+		if (userscore === matchingScore) {
       		wins++;
-        	$("#wins").text(wins);
-        	restart();
+			$("#wins").text(wins);
+			alert("Congratulations You Won!!")
+			restart();
       	}
-      	else if (userscore >= matchingscore) {
-      		lose++;
-        	$("#lose").text(lose);
+      	else if (userscore >= matchingScore) {
+      		losses++;
+			$("#lose").text(losses);
+			alert("Sorry You Lose, Please Try Again.")
         	restart();
       	}
     });
